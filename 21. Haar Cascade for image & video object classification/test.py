54654fbeb927311b927d6cb3ea6bfd7c92c92d38
TanushreeShaw <shawtanushree17@gmail.com>
2020-11-15 22:36:07 +0530

    OpenCV with Python for Image and Video Analysis

---
+++ 21. Haar Cascade for image & video object classification/test.py
@@ -0,0 +1,40 @@
+import cv2
+import numpy as np
+
+face_cascade = cv2.CascadeClassifier('frontalface.xml')
+eye_cascade = cv2.CascadeClassifier('eye.xml')
+smile_cascade = cv2.CascadeClassifier('smile.xml')
+
+cap = cv2.VideoCapture('video.mp4')
+
+while True:
+    ret, img = cap.read()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    #faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    faces = face_cascade.detectMultiScale(gray)
+
+    smiles = smile_cascade.detectMultiScale(gray,20,20)
+    
+    
+    for (x,y,w,h) in smiles:
+        #cv2.rectangle(img, (x,y), (x+w,y+h), (255,255,0), 2)
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        cv2.putText(img, "Smile", (x-w,y-h), font, 0.5, (0,255,255), 2, cv2.LINE_AA)
+
+
+    for (x,y,w,h) in faces:
+        cv2.rectangle(img, (x,y), (x+w,y+h), (255,0,0), 2)
+        roi_gray = gray[y:y+h,x:x+w]
+        roi_color = img[y:y+h,x:x+w]
+        eyes = eye_cascade.detectMultiScale(roi_gray)
+
+        for (ex,ey,ew,eh) in eyes:
+            cv2.rectangle(roi_color, (ex,ey), (ex+ew,ey+eh), (0,255,0), 2)
+
+    cv2.imshow('img', img)
+    k=cv2.waitKey(30) & 0xff
+    if k == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
