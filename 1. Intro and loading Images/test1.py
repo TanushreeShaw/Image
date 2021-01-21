0fbc725ee718eb9475479dc1cfc0fffe67e29625
Tanushree Shaw <63643049+TanushreeShaw@users.noreply.github.com>
2021-01-21 19:55:40 +0530

    Update test1.py

--- 1. Intro and loading Images/test1.py
+++ 1. Intro and loading Images/test1.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 img = cv2.imread('watchreal.jpg', cv2.IMREAD_GRAYSCALE)
 
 cv2.imwrite('watch.jpg', img)
+
 #IMREAD_COLOR = 1
 #IMREAD_UNCHANGED = -1
 cv2.imshow('image', img)
@@ -13,6 +14,7 @@ cv2.destroyAllWindows()
 
 #plt.imshow(img, cmap='gray', interpolation='bicubic')
 #plt.plot([50, 100], [80, 100],'c', linewidth=5 )
+
 #plt.show()
 
 cv2.imwrite('watch.jpg', img)
