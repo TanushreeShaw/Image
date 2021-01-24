fc222b28818b833836682491fe63ded459c69284
Tanushree Shaw <63643049+TanushreeShaw@users.noreply.github.com>
2021-01-24 19:58:56 +0530

    Update test.py

--- 5. Image arithmetics and Logic/test.py
+++ 5. Image arithmetics and Logic/test.py
@@ -3,7 +3,6 @@ import cv2
 
 img1 = cv2.imread('3D1.png')
 img2 = cv2.imread('3D3.png')
-
 #img2 = cv2.imread('3D2.png')
 
 rows, columns, channels = img2.shape
@@ -25,13 +24,11 @@ cv2.imshow('mask_inv', mask_inv)
 cv2.imshow('img1_bg', img1_bg)
 cv2.imshow('img2_fg', img2_fg)
 cv2.imshow('result', result)
-
 #cv2.imshow('mask', mask)
 
 #add=img1+img2
 #add = cv2.add(img1,img2)
 #weighted = cv2.addWeighted(img1, 0.6, img2, 0.4, 0)
-
 #cv2.imshow('weighted',weighted) 
 
 cv2.waitKey(0)
